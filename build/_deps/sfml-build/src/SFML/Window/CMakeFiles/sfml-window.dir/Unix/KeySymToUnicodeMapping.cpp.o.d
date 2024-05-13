e9bf2f15697d6f7aa528e46a1f5b75b9a93ce8d7
4lias <jkartkowski@gmail.com>
2024-05-13 13:13:41 +0200

    Revert "Delete build directory"
    This reverts commit 1e9986fe3592c460a84eb4bcda4b03e9c83acdab.

---
+++ build/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToUnicodeMapping.cpp.o.d
@@ -0,0 +1,6 @@
+_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToUnicodeMapping.cpp.o: \
+ /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToUnicodeMapping.cpp \
+ /usr/include/stdc-predef.h \
+ /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToUnicodeMapping.hpp \
+ /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/include/SFML/Config.hpp \
+ /usr/include/X11/X.h
