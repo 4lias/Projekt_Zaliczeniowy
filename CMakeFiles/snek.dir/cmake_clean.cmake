7b2ca54ced6c24378e4a675e4845aa212501485e
4lias <jkartkowski@gmail.com>
2024-05-13 13:13:52 +0200

    Revert "Delete CMakeFiles directory"
    This reverts commit 991a3ce545406d25a9036a142f049d64a5759fd8.

---
+++ CMakeFiles/snek.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snek.dir/src/board/board.cpp.o"
+  "CMakeFiles/snek.dir/src/board/board.cpp.o.d"
+  "CMakeFiles/snek.dir/src/controller/controller.cpp.o"
+  "CMakeFiles/snek.dir/src/controller/controller.cpp.o.d"
+  "CMakeFiles/snek.dir/src/snake/snake.cpp.o"
+  "CMakeFiles/snek.dir/src/snake/snake.cpp.o.d"
+  "CMakeFiles/snek.dir/src/ui/ui.cpp.o"
+  "CMakeFiles/snek.dir/src/ui/ui.cpp.o.d"
+  "bin/snek"
+  "bin/snek.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snek.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
