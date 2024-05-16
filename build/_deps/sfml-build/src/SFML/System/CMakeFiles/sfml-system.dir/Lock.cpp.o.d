09abc5251fc882ecf710d849789267565eabb530
Torgard-Mirdas <160980137+Torgard-Mirdas@users.noreply.github.com>
2024-05-16 10:41:33 +0200

    Delete build directory
    del

--- build/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.o.d
+++
@@ -1,8 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.o: \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/src/SFML/System/Lock.cpp \
- /usr/include/stdc-predef.h \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/include/SFML/System/Lock.hpp \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/include/SFML/System/Export.hpp \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/include/SFML/Config.hpp \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/include/SFML/System/NonCopyable.hpp \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/include/SFML/System/Mutex.hpp
