45f8a633929513765b973424bfa3684b68348413
Torgard-Mirdas <160980137+Torgard-Mirdas@users.noreply.github.com>
2024-05-16 11:24:28 +0200

    Delete build directory
    fix

--- build/CMakeFiles/snek.dir/src/snake/snake.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/snek.dir/src/snake/snake.cpp.o: \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/src/snake/snake.cpp \
- /usr/include/stdc-predef.h \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/src/snake/snake.h
--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImpl.cpp.o.d
+++
@@ -1,7 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImpl.cpp.o: \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/src/SFML/Graphics/RenderTextureImpl.cpp \
- /usr/include/stdc-predef.h \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/src/SFML/Graphics/RenderTextureImpl.hpp \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/include/SFML/System/NonCopyable.hpp \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/include/SFML/System/Export.hpp \
- /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/build/_deps/sfml-src/include/SFML/Config.hpp
