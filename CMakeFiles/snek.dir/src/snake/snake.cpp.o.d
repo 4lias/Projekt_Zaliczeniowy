f8c4fc59d3c341ce6157fd47bd8f4cf4e0ffe49b
4lias <jkartkowski@gmail.com>
2024-05-16 11:15:54 +0200

    Revert "Delete CMakeFiles directory"
    This reverts commit 229cb8221d555563998f7c9153f1e235edfb1885.

---
+++ CMakeFiles/snek.dir/src/snake/snake.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/snek.dir/src/snake/snake.cpp.o: \
+ /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/src/snake/snake.cpp \
+ /usr/include/stdc-predef.h \
+ /home/alias/Documents/GitHub/Projekt_Zaliczeniowy/src/snake/snake.h
